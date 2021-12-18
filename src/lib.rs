e68a263bfa0540c795fb4249e6c801b507c9d455
elias <maierelias13@gmail.com>
2021-12-18 17:26:34 +0100

    struggle with lifetime

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,5 @@
-use std::collections::HashMap;
-
 use bool_algebra::bool_to_u32;
+use std::collections::HashMap;
 
 pub struct Error {
     msg: String,
@@ -82,6 +81,59 @@ trait Component {
     fn name(&self) -> String;
 }
 
+trait IODevice {
+    fn set(&mut self, in_name: &str, value: bool, max_vised: u8) -> Result<(), Error>;
+    fn get(&mut self, out_name: &str, max_vised: u8) -> Result<bool, Error>;
+    fn in_names(&self) -> Vec<String>;
+    fn out_names(&self) -> Vec<String>;
+}
+
+struct InOut {
+    name: String,
+    viseted: u8,
+    value: bool,
+}
+
+impl InOut {
+    fn new(name: &str) -> Self {
+        Self {
+            name: name.to_string(),
+            viseted: 0,
+            value: false,
+        }
+    }
+}
+
+impl IODevice for InOut {
+    fn set(&mut self, in_name: &str, value: bool, max_vised: u8) -> Result<(), Error> {
+        if in_name != self.name {
+            todo!();
+        }
+        if self.viseted > max_vised {
+            todo!();
+        }
+        self.viseted += 1;
+        self.value = value;
+        Ok(())
+    }
+    fn get(&mut self, out_name: &str, max_vised: u8) -> Result<bool, Error> {
+        if out_name != self.name {
+            todo!();
+        }
+        if self.viseted > max_vised {
+            todo!();
+        }
+        self.viseted += 1;
+        Ok(self.value)
+    }
+    fn in_names(&self) -> Vec<String> {
+        vec![self.name.clone()]
+    }
+    fn out_names(&self) -> Vec<String> {
+        vec![self.name.clone()]
+    }
+}
+
 // #[derive(Debug, Clone, PartialEq)]
 struct Entry<'a> {
     viseted: u8,
@@ -104,20 +156,34 @@ impl<'a> Entry<'a> {
             out_map,
         })
     }
+}
 
+impl<'a> IODevice for Entry<'a> {
     fn set(&mut self, in_name: &str, value: bool, max_vised: u8) -> Result<(), Error> {
-        // TODO max_vised error
+        if self.viseted > max_vised {
+            // TODO
+            todo!();
+        }
         if let Some(name) = self.in_map.get(in_name) {
             self.viseted += 1;
             self.component.set(name, value)
-        }else{
+        } else {
+            // TODO
             todo!();
         }
     }
-    fn get(&mut self, out_names: &str, max_vised: u8) -> Result<bool, Error> {
-        // TODO max_vised error
-        self.viseted += 1;
-        self.component.get(out_names)
+    fn get(&mut self, out_name: &str, max_vised: u8) -> Result<bool, Error> {
+        if self.viseted > max_vised {
+            // TODO
+            todo!();
+        }
+        if let Some(name) = self.out_map.get(out_name) {
+            self.viseted += 1;
+            self.component.get(name)
+        } else {
+            // TODO
+            todo!();
+        }
     }
 
     fn in_names(&self) -> Vec<String> {
@@ -127,7 +193,7 @@ impl<'a> Entry<'a> {
         }
         result
     }
-    fn out_namess(&self) -> Vec<String> {
+    fn out_names(&self) -> Vec<String> {
         let mut result = Vec::new();
         for key in self.out_map.keys() {
             result.push(key.clone());
@@ -136,17 +202,94 @@ impl<'a> Entry<'a> {
     }
 }
 
+struct Connection {
+    in_map: HashMap<String, String>,
+    out_map: HashMap<String, String>,
+    comp_name: String,
+}
+
+impl Connection {
+    fn new(comp_name: &str, in_tuple: Vec<(&str, &str)>, out_tuple: Vec<(&str, &str)>) -> Self {
+        let mut in_map = HashMap::new();
+        for (key, val) in in_tuple {
+            in_map.insert(key.to_string(), val.to_string());
+        }
+
+        let mut out_map = HashMap::new();
+        for (key, val) in out_tuple {
+            out_map.insert(key.to_string(), val.to_string());
+        }
+
+        Self {
+            comp_name: comp_name.to_string(),
+            out_map,
+            in_map,
+        }
+    }
+}
+
 struct Circuit<'a> {
     in_names: Vec<String>,
-    out_namess: Vec<String>,
+    out_names: Vec<String>,
     /// a graph containing the index to the components
     compute_graph: Vec<Vec<usize>>,
-    components: Vec<Entry<'a>>,
+    components: Vec<&'a mut dyn IODevice>,
     name: String,
 }
 
 impl<'a> Circuit<'a> {
-    fn new() {}
+    fn new(
+        in_names: Vec<&str>,
+        out_names: Vec<&str>,
+        name: &str,
+        connections: Vec<Connection>,
+        all_components: HashMap<String, &'a dyn Component>,
+    ) -> Result<Self, Error> {
+        // generate input and output nodes
+        // comp_map is to look up the index in the components vec by the name
+
+        let mut comp_map = HashMap::new();
+        let mut io = Vec::new();
+        for in_name in in_names.clone() {
+            comp_map.insert(in_name.to_string(), io.len());
+            io.push(InOut::new(in_name));
+        }
+        for out_name in out_names.clone() {
+            comp_map.insert(out_name.to_string(), io.len());
+            io.push(InOut::new(out_name));
+        }
+
+        let mut components: Vec<&'a mut (dyn IODevice + 'a)> = Vec::new();
+        for in_out in io {
+            components.push(&'a mut in_out.to_owned());
+        }
+
+        let in_names: Vec<String> = in_names
+            .iter()
+            .map(|s| -> String { s.to_string() })
+            .collect();
+        let out_names: Vec<String> = out_names
+            .iter()
+            .map(|s| -> String { s.to_string() })
+            .collect();
+        let name = name.to_string();
+
+        // insert components
+
+        let mut compute_graph = Vec::new();
+
+        Ok(Self {
+            in_names,
+            out_names,
+            compute_graph,
+            components,
+            name,
+        })
+    }
+
+    fn opt(&mut self) {
+        todo!();
+    }
 }
 
 impl<'a> Component for Circuit<'a> {
@@ -160,7 +303,7 @@ impl<'a> Component for Circuit<'a> {
         self.in_names.clone()
     }
     fn out_names(&self) -> Vec<String> {
-        self.out_namess.clone()
+        self.out_names.clone()
     }
     fn name(&self) -> String {
         self.name.clone()
